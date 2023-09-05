26859038d289e862bb8949c6053aec17805b62b4
Adam Jamiu <adismail4@gmail.com>
2023-09-05 20:52:47 +0100

    Add interactive state

--- main.js
+++ main.js
@@ -1,4 +1,48 @@
+const container = document.querySelector(".container");
+const wrapper = document.querySelector(".wrapper");
 const submit = document.querySelector(".submit");
-const selectedNumber = document.querySelector(".number");
+const numbers = document.querySelectorAll(".number");
 
-submit.addEventListener("click", console.log("clicked"));
+// const userRateValue = () => {
+//   numbers.forEach((number) =>
+//     number.addEventListener("click", () => number.innerHTML)
+//   );
+// };
+
+numbers.forEach((number) =>
+  number.addEventListener("click", () => console.log(number.innerHTML))
+);
+
+const interactiveState = () => {
+  const ratedContainer = document.createElement("div");
+  ratedContainer.classList.add("thank-you");
+
+  const image = document.createElement("img");
+  image.src = "./images/illustration-thank-you.svg";
+  image.alt = "thank-you svg";
+
+  ratedContainer.appendChild(image);
+
+  const userSelected = document.createElement("p");
+  userSelected.innerHTML = numbers.forEach((number) =>
+    number.addEventListener("click", () => number.innerHTML)
+  ); //userRateValue(); //`You selected ${userRateValue()} out of 5`;
+  ratedContainer.appendChild(userSelected);
+
+  const thank = document.createElement("p");
+  thank.innerHTML = "Thank you!";
+  ratedContainer.appendChild(thank);
+
+  const appreciate = document.createElement("p");
+  appreciate.innerHTML =
+    "We appreciate you taking the time to give a rating. If you ever need more support,don’t hesitate to get in touch!";
+  ratedContainer.appendChild(appreciate);
+
+  container.appendChild(ratedContainer);
+};
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  container.removeChild(wrapper);
+  interactiveState();
+});
